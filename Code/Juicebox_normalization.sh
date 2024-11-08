56d00abe25f00b50c979d67a43b178300238c83e
DennisHeck <55958437+DennisHeck@users.noreply.github.com>
2024-11-08 15:17:34 +0100

    Add files via upload
    Add bash script that allows the selection of the juicer normalization method

---
+++ Code/Juicebox_normalization.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -e
+
+# Part of STARE: https://github.com/SchulzLab/STARE
+# Small bash script to ease the normalization of .hic-files with Juicebox (https://github.com/aidenlab/juicer/wiki/Data-Extraction)
+
+help="\n
+Usage: ./Juicebox_KR_normalization.sh [-h hic-file to normalize]\n[-j path to the jar-file]\n
+[-d folder to write the normalized files to]\n
+optional:\n
+[-c the chromosomes to normalize, e.g. 1-22+XY, default is 1-22, also possible to query individual chromosomes or multiple ones comma separated: 1,5,7]\n
+[-b bin size, meaning resolution of the matrix (default 5000)]
+[-n Normalization approach (KR/VC/VC_SQRT, Default KR)]\n"
+
+# ------------------------------------------------------------------------------------------------------
+# FETCHING INPUT
+# ------------------------------------------------------------------------------------------------------
+hic_file=""
+out_folder=""
+jar_file=""
+chromosomes="1-22"
+bin_size=5000
+normalization="KR"
+
+# Parsing command line.
+while getopts "h:d:j:c:b:n:" o;
+do
+case $o in
+	h)	hic_file=$OPTARG;;
+	d)	out_folder=$OPTARG;;
+	j)	jar_file=$OPTARG;;
+	c)	chromosomes=$OPTARG;;
+  b)  bin_size=$OPTARG;;
+  n)  normalization=$OPTARG;;
+esac
+done
+
+if [ -z "$hic_file" ] ;
+then
+	echo Hi-C file must be specified with -h
+	exit 1;
+fi
+
+if [ -z "$out_folder" ] ;
+then
+	echo An output folder must be specified with -d
+	exit 1;
+fi
+
+if [ -z "$jar_file" ] ;
+then
+	echo The jar file is needed to call Juicebox
+	exit 1;
+fi
+
+# Create the out_dir if not existent and call Juicebox for each of the specified chromosomes.
+if [ ! -d "$out_folder" ]; then
+  mkdir "$out_folder"
+fi
+
+if [[ $chromosomes == *"-"* ]]; then
+  chr_range=$(echo ${chromosomes} | tr -d '+XY')
+  IFS='-' read -ra ADDR <<< "${chr_range}"
+  for i in $(seq "${ADDR[0]}" "${ADDR[1]}"); do
+    java -jar ${jar_file} dump observed "${normalization}" "${hic_file}" chr${i} chr${i} BP ${bin_size} ${out_folder}/chr${i}_"${normazliation}"_Contacts.txt;
+    gzip ${out_folder}/chr${i}_"${normalization}"_Contacts.txt;
+  done
+  # Manually check if any gonosome was selected in addition to the range.
+  if [[ $chromosomes == *"X"* ]]; then
+    java -jar ${jar_file} dump observed "${normalization}" ${hic_file} chrX chrX BP ${bin_size} ${out_folder}/chrX_"${normalization}"_Contacts.txt;
+    gzip ${out_folder}/chrX_"${normalization}"_Contacts.txt;
+  fi
+  if [[ $chromosomes == *"Y"* ]]; then
+    java -jar ${jar_file} dump observed "${normalization}" ${hic_file} chrY chrY BP ${bin_size} ${out_folder}/chrY_"${normalization}"_Contacts.txt;
+    gzip ${out_folder}/chrY_"${normalization}"_Contacts.txt;
+  fi
+elif [[ $chromosomes == *","* ]]; then
+    IFS=',' read -ra csv <<< "$chromosomes"
+    for i in "${csv[@]}"; do
+    java -jar ${jar_file} dump observed "${normalization}" ${hic_file} chr${i} chr${i} BP ${bin_size} ${out_folder}/chr${i}_"${normalization}"_Contacts.txt;
+    gzip ${out_folder}/chr${i}_"${normalization}"_Contacts.txt;
+    done
+else  # If only one individual chromosome was queried.
+    java -jar ${jar_file} dump observed "${normalization}" "${hic_file}" chr${chromosomes} chr${chromosomes} BP ${bin_size} ${out_folder}/chr${chromosomes}_"${normalization}"_Contacts.txt;
+    gzip ${out_folder}/chr${chromosomes}_"${normalization}"_Contacts.txt;
+fi
+
